522653ccc73321776277c6636592608cd494ed8e
Katherina Kaliaha <mellinda077@mail.ru>
2023-07-06 17:53:53 +0300

    change vite build path

--- universe/www/js/index.js
+++ universe/www/js/index.js
@@ -22,7 +22,7 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
-  // Cordova is now initialized. Have fun!
+  window.ref = cordova.InAppBrowser;
 
   console.log('Device is ready');
 }
