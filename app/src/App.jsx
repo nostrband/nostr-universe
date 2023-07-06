20cd28db74fbcb50e7995cbeb5eda13bfea85932
Katherina Kaliaha <mellinda077@mail.ru>
2023-07-06 17:52:10 +0300

    fix: change vite build path

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,20 +2,31 @@ import './App.css';
 
 const App = () => {
 
-  const open = () => {
-    var ref = cordova.InAppBrowser.open('https://nostr.band/', '_blank', 'location=yes');
+  const openBtn = () => {
+    console.log(window.ref)
+    const ref = window.ref.open('https://snort.social/global', '_blank', 'location=yes')
+
+    ref.executeScript({ file: "scriptApp.js" });
     ref.addEventListener('loadstart', () => {
-      console.log(Object.keys(window.nostr))
+      console.log(Object.keys(window.nostr), '!')
     });
+    ref.addEventListener('loadstop', function () {
+
+      //   ref.executeScript({
+      //     code: `(
+      //       function() {
+      //         console.log(window.nostr);
+      //         console.log(11111111);
+      //       })()` })
+    });
+
   }
-
-
   return (
     <div className="App">
       <header className="App-header">
-        <button className='btn' onClick={() => open()}
+        <button className='btn' onClick={openBtn}
         >
-          Go to https://nostr.band
+          Go to https://snort.social
         </button>
 
       </header>
