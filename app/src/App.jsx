1a81ae504230b13fbfdde5645245f14094382731
denis1989 <denis1989@bk.ru>
2023-07-09 18:51:00 +0300

    added changes

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,86 +2,34 @@ import './App.css';
 
 const App = () => {
 
-  const open = async () => {
-    var ref = cordova.InAppBrowser.open('https://nostr.band/', '_blank', 'location=yes');
-    /*ref.addEventListener('loadstart', async () => {
-      console.log(Object.keys(window.nostr))
-      console.log("DGFDGFDGDFGDF")
-      console.log(Object.keys(cordova.plugins.NostrKeyStore))
-      console.log("DGFDGFDGDFGDF1")
-    });*/
-
-
-    let NostrKeyStore = {
-      getPublicKey: async function () {
-        return new Promise((resolve, reject) => {
-          cordova.plugins.NostrKeyStore.getPublicKey(function (res) {
-            resolve(res.pubKey.replaceAll("\"", ""))
-          }, function (error) {
-            reject(error)
-          })
-        })
-      }, signEvent: function (msg) {
-        return new Promise((resolve, reject) => {
-          cordova.plugins.NostrKeyStore.signEvent(function (res) {
-            resolve(res)
-          }, function (error) {
-            reject(error)
-          }, msg)
-        })
-      }
-    };
-
-    const utils = await fetch("./cordova.js");
-    const utils1 = await fetch("./cordova_plugins.js");
-    const utils2 = await fetch("./plugins/cordova-plugin-device/www/device.js");
-    const utils3 = await fetch("./plugins/cordova-plugin-inappbrowser/www/inappbrowser.js");
-    const utils4 = await fetch("./plugins/nostr-keystore-cordova-plugin/www/nostr.js");
-
-
-
-    const utilsText = await utils.text();
-    const utilsText1 = await utils1.text();
-    const utilsText2 = await utils2.text();
-    const utilsText3 = await utils3.text();
-    const utilsText4 = await utils4.text();
-
-
-
+  const open = async (url) => {
+    var ref = cordova.InAppBrowser.open(url, '_blank', 'location=yes');
 
     ref.addEventListener('loadstop', async (event) => {
-      // ref.executeScript({
-      //   code: utilsText + '; ' + utilsText1 + '; ' + utilsText2 + '; ' + utilsText3 + '; ' + utilsText4 + '; let NostrKeyStore = { getPublicKey: function () {return new Promise((resolve, reject) => { cordova.plugins.NostrKeyStore.getPublicKey( function (res) { resolve(res.pubKey.replaceAll("\\"", "")); console.log(`key`); },  function (error) {reject(error)})})}, signEvent: function (msg) {return new Promise((resolve, reject) => {cordova.plugins.NostrKeyStore.signEvent(function (res) {resolve(res)}, function (error) {reject(error)}, msg)})}}; window.nostr = NostrKeyStore;'
-      // }, function () {
-      //   console.log('script injected');
-      // });
-      ref.executeScript({
-        code: 'let NostrKeyStore = { getPublicKey: function () {webkit.messageHandlers.cordova_iab.postMessage(JSON.stringify({my_message: "this is the message"}));}, signEvent: function (msg) {return new Promise((resolve, reject) => {cordova.plugins.NostrKeyStore.signEvent(function (res) {resolve(res)}, function (error) {reject(error)}, msg)})}}; window.nostr = NostrKeyStore;'
-      }, function () {
-        console.log('script injected');
-      });
 
-      console.log("DGFDGFDGDFGDFloadstop2")
+      ref.executeScript({
+        code: 'window.localStorage.setItem("nostrPluginResponse", ""); let startDateNostrPluginResponse = Date.now(); let timeoutNostrPluginResponse = 100000; let NostrKeyStore = { getPublicKey: function () {webkit.messageHandlers.cordova_iab.postMessage(JSON.stringify({my_message: "this is the message"})); return new Promise(waitForFoo);}, signEvent: function (msg) {return new Promise((resolve, reject) => {cordova.plugins.NostrKeyStore.signEvent(function (res) {resolve(res)}, function (error) {reject(error)}, msg)})}}; window.nostr = NostrKeyStore; console.log("BEFORE"); function waitForFoo(resolve, reject) { let nostrPluginResponse = window.localStorage.getItem("nostrPluginResponse"); console.log(nostrPluginResponse); if (nostrPluginResponse !== "") {console.log("nostrPluginResponse is empty"); resolve(nostrPluginResponse);} else if (timeoutNostrPluginResponse && (Date.now() - startDateNostrPluginResponse) >= timeoutNostrPluginResponse) { console.log("nostrPluginResponse rejected"); reject(new Error("timeout"));} else { console.log("nostrPluginResponse new timeOut"); setTimeout(waitForFoo.bind(this, resolve, reject), 1000);}}'
+      }, function () {
+        console.log('script injected window nostr');
+      });
     });
 
-    // ref.show();
     ref.addEventListener('message', async (params) => {
       console.log('start getPublicKey')
 
       if (params.data.my_message === 'this is the message') {
 
         let publicKey = await window.nostr.getPublicKey()
+        publicKey = publicKey.replaceAll("\"", "")
 
-        // ref.executeScript({
-        //   code: "\
-        //   var message = '2ab0beeb48b98962edaed475c83305d2f5af5baec07f0c1291379a3703672634';\
-        //   var messageObj = {my_message: message};\
-        //   var stringifiedMessageObj = JSON.stringify(messageObj);\
-        //   webkit.messageHandlers.cordova_iab.postMessage(stringifiedMessageObj);"
-        // },
-        //   function () {
-        //     console.log('script injected1111');
-        //   })
+        console.log('publicKey from plugin ' + publicKey);
+        let script = 'console.log(window.localStorage.getItem("nostrPluginResponse") + " firstNostrPluginResponse"); window.localStorage.setItem("nostrPluginResponse", "' + publicKey + '"); console.log(window.localStorage.getItem("nostrPluginResponse") + " secondNostrPluginResponse");'
+        console.log('script ' + script);
+        ref.executeScript({
+          code: script
+        }, function () {
+          console.log('script injected publicKey');
+        });
       }
 
       console.log("message received: " + JSON.stringify(params.data))
@@ -89,20 +37,28 @@ const App = () => {
 
   }
 
-  function executeScriptCallBack(obj) {
-    console.log("DGFDGFDGDFGDFloadstop3")
-    console.log(JSON.stringify(obj))
-    console.log("DGFDGFDGDFGDFloadstop4")
-  }
-
-
-
   return (
     <div className="App">
       <header className="App-header">
-        <button className='btn' id='button1' onClick={() => open()}
+        <button className='btn' id='button1' onClick={() => open('https://nostr.band/')}
         >
           Go to https://nostr.band/
+        </button>
+        <button className='btn' id='button1' onClick={() => open('https://snort.social/')}
+        >
+          Go to https://snort.social/
+        </button>
+        <button className='btn' id='button1' onClick={() => open('https://iris.to/')}
+        >
+          Go to https://iris.to/
+        </button>
+        <button className='btn' id='button1' onClick={() => open('https://coracle.social/')}
+        >
+          Go to https://coracle.social/
+        </button>
+        <button className='btn' id='button1' onClick={() => open('https://satellite.earth/')}
+        >
+          Go to https://satellite.earth/
         </button>
 
       </header>
