b6efeda3fb9b4184545ba2f1ae86fef2dcf0eff1
Katherina Kaliaha <mellinda077@mail.ru>
2023-07-16 16:14:03 +0300

    feat: add UI

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,18 +1,109 @@
 import { nip19 } from 'nostr-tools';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
+
+import { AiOutlineClose, AiOutlineSearch } from "react-icons/ai";
+import { BiSolidPencil } from "react-icons/bi";
+import { IoMdArrowDropdown } from "react-icons/io";
+import { MdOutlineDone } from "react-icons/md";
+
+import { Modal } from './components/ModalWindow';
+import { IconBtn } from './components/iconBtn';
+import avatar from './icons/avatar.png';
+import coracleIcon from './icons/coracle.png';
+import irisIcon from './icons/iris.png';
+import nostrIcon from './icons/nb.png';
+import satelliteIcon from './icons/satellite.png';
+import snortIcon from './icons/snort.png';
+
+const apps = [{ title: 'Nostr', img: nostrIcon, link: 'https://nostr.band/' },
+{ title: 'Snort', img: snortIcon, link: 'https://snort.social/' },
+{ title: 'Iris', img: irisIcon, link: 'https://iris.to/' },
+{ title: 'Coracle', img: coracleIcon, link: 'https://coracle.social/' },
+{ title: 'Satellite', img: satelliteIcon, link: 'https://satellite.earth/' }];
+
+
 
 const App = () => {
   const [npub, setNpub] = useState('');
-  const storage = window.localStorage;
+  const [isOpenKeysList, setIsOpenKeysList] = useState(false);
+  const [modalActive, setModalActive] = useState(false);
+  const [keys, setKeys] = useState();
+  const [openKey, setOpenKey] = useState();
+  const [list, setList] = useState();
+  const ref = useRef();
 
-  useEffect(() => {
-    const key = storage.getItem('npub');
+  function getListKeys() {
+    return new Promise((resolve, reject) => {
+      cordova.plugins.NostrKeyStore.listKeys(
+        function (res) {
+          resolve(res)
+        },
+        function (error) {
+          reject(error)
+        }
+      )
+    })
+  }
 
-    if (key) {
-      setNpub(key.replace(/"/g, ''))
+  useEffect(async () => {
+    document.addEventListener("deviceready", onDeviceReady, false)
+
+    async function onDeviceReady() {
+      console.log('device ready');
+      const list = await getListKeys();
+      if (list.currentAlias) {
+        const currentKey = nip19.npubEncode(list.currentAlias).replace(/"/g, '');
+        setNpub(currentKey);
+        setList(list);
+        const keys = Object.keys(list).filter((key) => key !== 'currentAlias');
+        if (keys.length) {
+          setKeys(keys)
+        }
+      } else {
+        setNpub('Key is not chosen')
+      }
+      // }
+      console.log(JSON.stringify(list))
     }
   }, [])
+  const openListKeys = async () => {
+    setIsOpenKeysList((prev) => !prev)
+  }
+
+  const addKey = async () => {
+    const key = await promiseAddKey();
+    if (key) {
+      const list = await getListKeys();
+      if (list.currentAlias) {
+        const currentKey = nip19.npubEncode(list.currentAlias).replace(/"/g, '');
+        setList(list);
+        if (currentKey !== npub) {
+          setNpub(currentKey);
+        }
+        const keys = Object.keys(list).filter((key) => key !== 'currentAlias');
+        if (keys.length) {
+          // const infoKeys = keys.map((key) => nip19.npubEncode(key).replace(/"/g, ''))
+          setKeys(keys)
+        }
+      }
+    }
+    console.log(JSON.stringify(list))
+
+  }
+
+  function promiseAddKey() {
+    return new Promise((resolve, reject) => {
+      cordova.plugins.NostrKeyStore.addKey(
+        function (res) {
+          resolve(res)
+        },
+        function (error) {
+          reject(error)
+        }
+      )
+    })
+  }
 
   const open = async (url) => {
     var ref = cordova.InAppBrowser.open(url, '_blank', 'location=yes');
@@ -58,7 +149,6 @@ const App = () => {
       const reply = await window.nostr[method](params.data.params);
       if (method === 'getPublicKey') {
         let npub = nip19.npubEncode(reply);
-        storage.setItem('npub', JSON.stringify(npub));
         setNpub(npub.replace(/"/g, ''))
       }
       const jsonReply = JSON.stringify(reply);
@@ -81,33 +171,152 @@ const App = () => {
 
   }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        {npub && <div className='key'>Key: {npub}</div>}
-        <button className='btn' id='button1' onClick={() => open('https://nostr.band/')}
-        >
-          Go to https://nostr.band/
-        </button>
-        <button className='btn' id='button1' onClick={() => open('https://snort.social/')}
-        >
-          Go to https://snort.social/
-        </button>
-        <button className='btn' id='button1' onClick={() => open('https://iris.to/')}
-        >
-          Go to https://iris.to/
-        </button>
-        <button className='btn' id='button1' onClick={() => open('https://coracle.social/')}
-        >
-          Go to https://coracle.social/
-        </button>
-        <button className='btn' id='button1' onClick={() => open('https://satellite.earth/')}
-        >
-          Go to https://satellite.earth/
-        </button>
+  const editBtnClick = (ev) => {
+    const index = ev.target.dataset.key;
+    setOpenKey(keys[index])
+    setModalActive(true)
+  }
 
+  async function copyKey() {
+    const text = list[openKey].publicKey
+
+    await cordova.plugins.clipboard.copy(text, (val) => {
+      console.log('I success', val)
+    }, () => {
+      console.log('I err')
+    });
+  }
+
+  const showKey = async () => {
+    const key = await promiseShowKey({ publicKey: openKey })
+    console.log(JSON.stringify(key))
+  }
+
+  function promiseShowKey(msg) {
+    return new Promise((resolve, reject) => {
+      cordova.plugins.NostrKeyStore.showKey(
+        function (res) {
+          console.log(JSON.stringify(res), 'I work')
+          resolve(res)
+        },
+        function (error) {
+          reject(error)
+        },
+        msg
+      )
+    })
+  }
+
+  const editKey = async () => {
+    console.log('value', ref.current.value)
+    const keysList = await promiseEditKey({ publicKey: openKey, name: ref.current.value })
+
+    if (keysList) {
+
+      setList(keysList);
+      const keys = Object.keys(keysList).filter((key) => key !== 'currentAlias');
+      if (keys.length) {
+        setKeys(keys)
+      }
+    }
+  }
+
+  function promiseEditKey(msg) {
+    return new Promise((resolve, reject) => {
+      cordova.plugins.NostrKeyStore.editKey(
+        function (res) {
+          console.log(JSON.stringify(res), 'I work')
+          resolve(res)
+        },
+        function (error) {
+          reject(error)
+        },
+        msg
+      )
+    })
+  }
+
+  const submit = (ev) => {
+    ev.preventDefault();
+    console.log(ev.target.search.value)
+  }
+
+  return (
+    <>
+      <header className="header">
+        <img src={avatar} alt='avatar image' className='iconImg' />
+        <div className='keyInfo'>
+          <div className='key'>{npub}</div>
+          <button className='dropDown' onClick={openListKeys}>
+            <IoMdArrowDropdown color='white' size={24} className='iconDropDown' />
+          </button>
+        </div>
+        <div className={isOpenKeysList ? 'keysList isOpen' : 'keysList'}>
+          <ul className='list'>
+            {keys && keys.length && keys.map((key) => nip19.npubEncode(key).replace(/"/g, '')).map((key, ind) => {
+              return (<li className='listItem itemKey' key={key}>
+                <img src={avatar} alt='avatar image' className='iconImg' />
+                <div className='key'>{key}</div>
+                <button className='editBtn' data-key={ind} onClick={(ev) => editBtnClick(ev)}>
+                  <BiSolidPencil color='white' size={20} className='iconDropDown' />
+                </button>
+              </li>)
+            }
+            )
+            }
+
+            <li className='listItem btn'>
+              <button className='addBtn' onClick={addKey}>
+                + Add keys
+              </button>
+            </li>
+          </ul>
+        </div>
       </header>
-    </div>
+      <div className="wrapper">
+        <form className='form' onSubmit={(ev) => submit(ev)}>
+          <input className='input' name='search' type="search" placeholder="Search..." />
+          <button className="button" type='submit'><AiOutlineSearch color='gray' size={20} className='iconDropDown' /></button>
+        </form>
+        <section className='section'>
+          <h3>Apps</h3>
+          <div className='contentWrapper'>
+            {apps.map((app) => <IconBtn key={app.title} data={app} onClick={() => open(app.link)} />)}
+          </div>
+        </section>
+      </div>
+      <Modal activeModal={modalActive} setActive={setModalActive}>
+        {modalActive && (<div>
+          <div className='modalTitle'>
+            <h2>Edit key</h2>
+            <button className='dropDown' onClick={() => setModalActive(false)}>
+              <AiOutlineClose color='white' size={24} className='iconDropDown' />
+            </button>
+          </div>
+          <div className='modalKey'>
+            {nip19.npubEncode(list[openKey].publicKey).replace(/"/g, '')}
+          </div>
+          <div className='modalBtnWrapper'>
+            <button className='addBtn' onClick={copyKey}>Copy</button>
+            <button className='addBtn' onClick={showKey}>Show secret</button>
+          </div>
+          <div>
+            <h2 className='modalTitle'>Attributes</h2>
+          </div>
+          <div className='attributeWrapper'>
+            <span>Name: </span>
+            <input type='text' ref={ref} defaultValue={list[openKey].name} />
+            <button className='editBtn' onClick={editKey}>
+              <MdOutlineDone color='white' size={20} className='iconDropDown' />
+            </button>
+          </div>
+          <div>
+            <h2 className='modalTitle'>Profile</h2>
+          </div>
+        </div>
+        )}
+      </Modal >
+    </>
   );
 };
 
--- universe/www/js/index.js
+++ universe/www/js/index.js
@@ -20,9 +20,9 @@
 
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener("deviceready", onDeviceReady, false)
+// document.addEventListener("deviceready", onDeviceReady, false)
 
-function onDeviceReady() {
-  console.log('device ready');
-}
+// function onDeviceReady() {
+//   console.log('device ready');
+// }
 
