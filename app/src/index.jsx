ea7fe89b18fc94ebf5dc443c230e0dad6efafc1b
Bekbolsun <bekbolsun.azamatov.1579.7@gmail.com>
2023-08-01 22:03:40 +0600

    fix router bug

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { AppContextProvider } from "./store/app-context";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AppContextProvider>
         <App />
       </AppContextProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
