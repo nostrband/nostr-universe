d4f98fde2f033d17a20e92ba19a5799627aaa155
artur <brugeman.artur@gmail.com>
2023-08-23 07:43:50 +0300

    Fix loading of suggested profiles, fix height of note box

--- app/src/pages/Main.Page.jsx
+++ app/src/pages/Main.Page.jsx
@@ -29,6 +29,7 @@ const MainPage = () => {
     setOpenAddr,
     updateLastContact,
     currentTab,
+    currentWorkspace
   } = contextData || {};
 
   const [searchParams, setSearchParams] = useSearchParams();
@@ -92,7 +93,9 @@ const MainPage = () => {
 
         <TrendingProfiles onOpenProfile={setOpenAddr} />
         <TrendingNotes onOpenNote={setOpenAddr} />
-        <TrendingProfiles onOpenProfile={setOpenAddr} suggested />
+        {currentWorkspace?.suggestedProfiles.length > 0 && 
+	  <TrendingProfiles onOpenProfile={setOpenAddr} suggested />
+	}
         <AppsList />
         <ContactList onOpenProfile={setContactOpenAddr} />
 
