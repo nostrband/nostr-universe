a366a1e3e85fb5f43ed8e8b041084e1585ff6edf
Bekbolsun <bekbolsun.azamatov.1579.7@gmail.com>
2023-09-01 21:22:35 +0600

    save changes

--- app/src/components/perms/PermRequestModal.jsx
+++ app/src/components/perms/PermRequestModal.jsx
@@ -1,14 +1,21 @@
 import { useContext, useState, useEffect } from "react";
 import { AppContext } from "../../store/app-context";
 import { SecondaryCloseIcon } from "../../assets";
-import { IconButton as MUIconButton, Button, Switch, FormControlLabel, styled } from "@mui/material";
+import {
+  IconButton as MUIconButton,
+  Button,
+  Switch,
+  FormControlLabel,
+  styled,
+  Container,
+} from "@mui/material";
 import { Modal } from "../UI/modal/Modal";
-import { PinItem } from "../pins/PinItem";
 import { IconButton } from "../UI/IconButton";
 
 export const PermRequestModal = ({ isOpen, onClose }) => {
   const contextData = useContext(AppContext);
-  const { currentPermRequest, getTab, replyCurrentPermRequest } = contextData || {};
+  const { currentPermRequest, getTab, replyCurrentPermRequest } =
+    contextData || {};
 
   const [isRemember, setIsRemember] = useState(false);
 
@@ -17,28 +24,26 @@ export const PermRequestModal = ({ isOpen, onClose }) => {
   }, [currentPermRequest]);
 
   const req = currentPermRequest;
+  console.log(req, JSON.stringify(req), "REQ IN PermRequestModal component");
 
   const tab = getTab(req?.tabId);
 
   let label = "";
   let payload = null;
-  if (req?.perm === "pubkey")
-    label = "Read you public key";
+  if (req?.perm === "pubkey") label = "Read your public key";
   else if (req?.perm.startsWith("sign:")) {
-    label = "Sign event of kind " + req?.perm.split(':')[1];
+    label = "Sign event of kind " + req?.perm.split(":")[1];
     payload = JSON.stringify(req.event);
-  }
-  else if (req?.perm === "encrypt") {
+  } else if (req?.perm === "encrypt") {
     label = "Encrypt a message";
     // FIXME add pubkey
     payload = req.plainText;
-  }
-  else if (req?.perm === "decrypt") {
+  } else if (req?.perm === "decrypt") {
     label = "Decrypt a message";
     // FIXME add pubkey
     payload = req.cipherText;
-  }  
-  
+  }
+
   const onDisallow = async () => {
     await replyCurrentPermRequest(false, isRemember);
   };
@@ -46,112 +51,79 @@ export const PermRequestModal = ({ isOpen, onClose }) => {
   const onAllow = async () => {
     await replyCurrentPermRequest(true, isRemember);
   };
-  
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <Container>
-	<header>
-          <h3>Permission request</h3>
+      <StyledContainer>
+        <StyledHeader>
+          <h1>Permission request</h1>
           <StyledIconButton onClick={onClose}>
             <SecondaryCloseIcon />
           </StyledIconButton>
-	</header>
-	{req && (
-	  <main>
-	    <center>
-	      <IconButton
-		data={{ title: tab.title, img: tab.icon }}
-		size="big"
-	      />
-	      
-	    </center>	    
+        </StyledHeader>
+        {req && (
+          <div>
+            <center>
+              <IconButton
+                data={{ title: tab.title, img: tab.icon }}
+                size="big"
+              />
+            </center>
 
-	    <RequestContainer>
-	      <h5>{label}</h5>
-	      <pre>
-		{payload}
-	      </pre>
-
+            <RequestContainer>
+              <h5>{label}</h5>
+              <pre>{payload}</pre>
 
               <FormControlLabel
-		control={
-		  <Switch checked={isRemember} onChange={(e) => setIsRemember(e.target.checked)} />
-		}
-		label="Remember, don't ask again"
+                control={
+                  <Switch
+                    checked={isRemember}
+                    onChange={(e) => setIsRemember(e.target.checked)}
+                  />
+                }
+                label="Remember, don't ask again"
               />
-	      
-	    </RequestContainer>
-	    
-	    <ButtonContainer>
-              <Button variant="contained" className="button" onClick={onDisallow}>
-		Disallow
+            </RequestContainer>
+
+            <ButtonContainer>
+              <Button
+                variant="contained"
+                className="button"
+                onClick={onDisallow}
+              >
+                Disallow
               </Button>
-              <Button variant="contained" className="button" color="success" onClick={onAllow}>
-		Allow
+              <Button
+                variant="contained"
+                className="button"
+                color="success"
+                onClick={onAllow}
+              >
+                Allow
               </Button>
-	    </ButtonContainer>
-	  </main>
-	)}
-      </Container>
+            </ButtonContainer>
+          </div>
+        )}
+      </StyledContainer>
     </Modal>
   );
 };
 
-const Container = styled("div")`
-  min-height: 100dvh;
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-template-rows: auto 1fr auto;
-  grid-template-areas:
-    "header"
-    "main";
-
-  & > header {
-    grid-area: header;
-    position: sticky;
-    background: #000;
-    z-index: 1199;
-    top: 0;
-    display: flex;
-    flex-direction: row;
-
-    & > h3 {
-      margin-left: 1rem;
-      margin-top: 0.3rem;
-      flex-grow: 1;
-    }
-  }
-
-  & > main {
-    grid-area: main;
-    overflow: scroll;
-    max-height: 100%;
-    padding-bottom: 2rem;
-  }
-`;
-
-const TabGroup = styled("div")(() => ({
+const StyledHeader = styled("div")(() => ({
   display: "flex",
-  flexDirection: "column",
-  marginTop: "0.5rem",
+  alignItems: "center",
+  justifyContent: "space-between",
+  h1: {
+    fontWeight: 500,
+    fontSize: "28px",
+  },
 }));
 
-
-const Title = styled("div")(() => ({
+const StyledContainer = styled(Container)(() => ({
+  paddingTop: "6px",
+  paddingBottom: "1rem",
   display: "flex",
-  flexDirection: "row",
-  margin: "0.5rem 1rem",
-  "& .label": {
-    overflow: "hidden",
-    textOverflow: "ellipsis",
-    display: "-webkit-box",
-    WebkitLineClamp: "1",
-    WebkitBoxOrient: "vertical",
-    flexGrow: "1",
-    marginLeft: "0.5rem",
-    marginTop: "0.5rem",
-    fontWeight: 600,
-  }
+  flexDirection: "column",
 }));
 
 const StyledIconButton = styled(MUIconButton)(() => ({
@@ -171,7 +143,7 @@ const RequestContainer = styled("div")(() => ({
     maxWidth: "100%",
     minHeight: "10rem",
     // FIXME make text wrap and add vertical scrolling
-  }
+  },
 }));
 
 const ButtonContainer = styled("div")(() => ({
@@ -192,5 +164,5 @@ const ButtonContainer = styled("div")(() => ({
     "&:hover, &:active": {
       background: "#363636",
     },
-  }
+  },
 }));
--- app/src/components/tabs/TabsModal.jsx
+++ app/src/components/tabs/TabsModal.jsx
@@ -1,14 +1,20 @@
 import { useContext } from "react";
 import { AppContext } from "../../store/app-context";
 import { SecondaryCloseIcon } from "../../assets";
-import { IconButton as MUIconButton, styled } from "@mui/material";
+import {
+  Container,
+  GlobalStyles,
+  IconButton as MUIconButton,
+  styled,
+} from "@mui/material";
 import { Modal } from "../UI/modal/Modal";
-import { PinItem } from "../pins/PinItem";
+
 import { IconButton } from "../UI/IconButton";
 
 export const TabsModal = ({ isOpen, onClose }) => {
   const contextData = useContext(AppContext);
-  const { currentWorkspace, getTab, onOpenTab, clearLastCurrentTab } = contextData || {};
+  const { currentWorkspace, getTab, onOpenTab, clearLastCurrentTab } =
+    contextData || {};
 
   const onClick = (tab) => {
     // to make sure onClose doesn't re-open the calling tab
@@ -17,84 +23,97 @@ export const TabsModal = ({ isOpen, onClose }) => {
     onOpenTab(tab);
   };
 
-  if (!currentWorkspace)
-    return;
-  
-  const tgs = Object.values(currentWorkspace.tabGroups).filter(tg => tg.tabs.length > 0);
+  if (!currentWorkspace) return;
+
+  const tgs = Object.values(currentWorkspace.tabGroups).filter(
+    (tg) => tg.tabs.length > 0
+  );
   tgs.sort((a, b) => b.lastActive - a.lastActive);
-  
+
+  const prepareTabs = (tabs) => {
+    return tabs
+      .map((id) => getTab(id))
+      .sort((a, b) => b.lastActive - a.lastActive);
+  };
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <Container>
-	<header>
-          <h3>Tabs</h3>
+      <StyledContainer>
+        <StyledHeader>
+          <h1>Tabs</h1>
           <StyledIconButton onClick={onClose}>
             <SecondaryCloseIcon />
           </StyledIconButton>
-	</header>
-	<main>
-	  {tgs.map(tg => {
-	    const tabs = tg.tabs.map(id => getTab(id));
-	    tabs.sort((a, b) => b.lastActive - a.lastActive);
-	    
-	    return (
-	      <TabGroup key={tg.id}>
-		<Title>
-		  <IconButton
-		    data={{ title: "", img: tg.info.icon }}
-		    size="small"
-		  />
-		  
-		  <div className="label">{tg.info.title}</div>
-		</Title>
-		<TabsContainer>
-		  {tabs.map(tab => (
-		    <Card key={tab.id} onClick={() => onClick(tab)}>
-		      <img src={tab.screenshot || ""} alt={tab.title} width="150" />
-		    </Card>
-		  ))}
-		</TabsContainer>
-	      </TabGroup>
-	    )
-	  })}
-	</main>
-      </Container>
+        </StyledHeader>
+        <>
+          {tgs.map((tg) => {
+            const tabs = prepareTabs(tg.tabs);
+            return (
+              <TabGroup key={tg.id}>
+                <Title>
+                  <StyledIconButton
+                    onClick={() => console.log("close tab group")}
+                    size="small"
+                  >
+                    <SecondaryCloseIcon />
+                  </StyledIconButton>
+                  <IconButton
+                    data={{ title: "", img: tg.info.icon }}
+                    size="small"
+                  />
+                  <div className="label">{tg.info.title}</div>
+                </Title>
+                <TabsContainer>
+                  {tabs.map((tab) => (
+                    <Card key={tab.id}>
+                      <StyledIconButton
+                        onClick={() =>
+                          console.log("close separate tab in tabgroup")
+                        }
+                        size="large"
+                        className="close_tab_btn"
+                      >
+                        <SecondaryCloseIcon />
+                      </StyledIconButton>
+                      <img
+                        src={tab.screenshot || tab.icon}
+                        alt={tab.title}
+                        width="150"
+                        onClick={() => onClick(tab)}
+                      />
+                    </Card>
+                  ))}
+                </TabsContainer>
+              </TabGroup>
+            );
+          })}
+        </>
+      </StyledContainer>
+      <GlobalStyles
+        styles={{
+          body: { overflow: isOpen ? "hidden !important" : "initial" },
+        }}
+      />
     </Modal>
   );
 };
 
-const Container = styled("div")`
-  min-height: 100dvh;
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-template-rows: auto 1fr auto;
-  grid-template-areas:
-    "header"
-    "main";
+const StyledContainer = styled(Container)(() => ({
+  paddingTop: "6px",
+  paddingBottom: "1rem",
+  display: "flex",
+  flexDirection: "column",
+}));
 
-  & > header {
-    grid-area: header;
-    position: sticky;
-    background: #000;
-    z-index: 1199;
-    top: 0;
-    display: flex;
-    flex-direction: row;
-
-    & > h3 {
-      margin-left: 1rem;
-      margin-top: 0.3rem;
-      flex-grow: 1;
-    }
-  }
-
-  & > main {
-    grid-area: main;
-    overflow: scroll;
-    max-height: 100%;
-    padding-bottom: 2rem;
-  }
-`;
+const StyledHeader = styled("div")(() => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+  h1: {
+    fontWeight: 500,
+    fontSize: "28px",
+  },
+}));
 
 const TabGroup = styled("div")(() => ({
   display: "flex",
@@ -102,11 +121,10 @@ const TabGroup = styled("div")(() => ({
   marginTop: "0.5rem",
 }));
 
-
 const Title = styled("div")(() => ({
   display: "flex",
   flexDirection: "row",
-  margin: "0.5rem 1rem",
+  margin: "0.5rem 0",
   "& .label": {
     overflow: "hidden",
     textOverflow: "ellipsis",
@@ -117,9 +135,9 @@ const Title = styled("div")(() => ({
     marginLeft: "0.5rem",
     marginTop: "0.5rem",
     fontWeight: 600,
-  }
+  },
 }));
-  
+
 const TabsContainer = styled("div")(() => ({
   display: "flex",
   flexDirection: "row",
@@ -127,9 +145,9 @@ const TabsContainer = styled("div")(() => ({
   overflow: "auto",
 }));
 
-// from TrendingProfileItem
 const Card = styled("div")(() => ({
   padding: "0.75rem",
+  paddingTop: "2rem",
   borderRadius: "24px",
   backgroundColor: "#222222",
   minHeight: "170px",
@@ -145,12 +163,22 @@ const Card = styled("div")(() => ({
   "& > img": {
     borderRadius: "1rem",
   },
+  position: "relative",
+  "& .close_tab_btn": {
+    position: "absolute",
+    right: 0,
+    top: 0,
+    width: 25,
+    height: 25,
+    transform: "translate(-25%,25%)",
+    padding: "2px",
+  },
 }));
 
 const StyledIconButton = styled(MUIconButton)(() => ({
   width: "44px",
   height: "44px",
-  "&:hover": {
+  "&:active": {
     backgroundColor: "rgba(251, 251, 251, 0.08)",
   },
 }));
