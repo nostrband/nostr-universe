8b13ad9c4587e80a39a0edc9c6fbd91e4e7a0cd3
a-fralou <afralou@yandex.by>
2023-09-30 17:58:56 +0300

    Fix pages

--- app/src/components/Modal/ModalAbout/ModalAboutContent.tsx
+++ app/src/components/Modal/ModalAbout/ModalAboutContent.tsx
@@ -1,3 +1,4 @@
+import { Typography } from '@mui/material'
 import { Container } from '@/layout/Container/Conatiner'
 import { StyledViewTitle, StyledWrap } from './styled'
 import { AppLogo } from '@/assets'
@@ -7,9 +8,10 @@ export const ModalAboutContent = () => {
   return (
     <Container>
       <StyledWrap>
-        <AppIcon size="big" isOutline={false} picture={AppLogo} />
+        <AppIcon isLight size="big" isOutline={false} picture={AppLogo} />
         <StyledViewTitle variant="h5">The Nostr Browser</StyledViewTitle>
         <StyledViewTitle variant="body1">0.2.0</StyledViewTitle>
+        <Typography variant="body1">some text</Typography>
       </StyledWrap>
     </Container>
   )
