9e1d3738c8216d0d8037d3877b5e9408ca83ccd7
artur <brugeman.artur@gmail.com>
2023-10-13 15:44:57 +0300

    Fix Apps feed layout, fix bottom navigation layout in smaller screens

---
+++ app/src/components/NavigationBottom/styled.tsx
@@ -0,0 +1,7 @@
+import { BottomNavigationAction, styled } from "@mui/material";
+
+export const StyledBottomNavigationAction = styled(BottomNavigationAction)(({ }) => ({
+  paddingLeft: '4px',
+  paddingRight: '4px',
+  minWidth: '70px'
+}))
