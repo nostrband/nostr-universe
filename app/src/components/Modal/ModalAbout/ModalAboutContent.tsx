74a5a9f67896907f8b00c9f13aff5482cc7187b1
artur <brugeman.artur@gmail.com>
2023-10-16 11:05:00 +0300

    Add log output to ModalAbout

--- app/src/components/Modal/ModalAbout/ModalAboutContent.tsx
+++ app/src/components/Modal/ModalAbout/ModalAboutContent.tsx
@@ -1,10 +1,28 @@
 import { Typography } from '@mui/material'
 import { Container } from '@/layout/Container/Conatiner'
-import { StyledViewTitle, StyledWrap } from './styled'
+import { StyledLogs, StyledViewTitle, StyledWrap } from './styled'
 import { AppLogo } from '@/assets'
 import { AppIcon } from '@/shared/AppIcon/AppIcon'
+import { useEffect, useState } from 'react'
 
 export const ModalAboutContent = () => {
+
+  const [logs, setLogs] = useState<string>('')
+
+  useEffect(() => {
+    // @ts-ignore
+    if (window.NativeLogs) {
+      // @ts-ignore
+      window.NativeLogs.getLog(100, false, (logs) => {
+        setLogs(logs)
+      })
+    } else {
+      setLogs(`Some random long string of text for testing purposes.\n
+      And then here is the second row, let's hope it looks better now.
+      `)
+    }
+  }, [])
+
   return (
     <Container>
       <StyledWrap>
@@ -12,6 +30,9 @@ export const ModalAboutContent = () => {
         <StyledViewTitle variant="h5">The Nostr Browser</StyledViewTitle>
         <StyledViewTitle variant="body1">0.4.0</StyledViewTitle>
         <Typography variant="body1">Spring is an open-source project by Nostr.Band.</Typography>
+        {logs && (
+          <StyledLogs readOnly={true}>{logs}</StyledLogs>
+        )}
       </StyledWrap>
     </Container>
   )
